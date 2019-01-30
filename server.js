457df306307af18f8ccaec00cd6806fd4a49ba4c
Arunish <arunishsharma@gmail.com>
2019-01-31 01:32:27 +0530

    Start start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
 
 var app=express();
 
@@ -30,13 +31,6 @@ app.get('/',(request,response)=>{
     welcomeMessage:'Hey! Welcome to the home page'
   });
 });
-
-
-
-
-
-
-
 app.get('/about',(request,response)=>{
   response.render('about.hbs',{
     pageTitle:'About Page',
@@ -50,4 +44,6 @@ app.get('/bad',(req,res)=>{
   });
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+  console.log(`Server is up on port ${port}`);
+});
